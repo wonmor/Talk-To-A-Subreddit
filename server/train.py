0e41a4f450c2e303e982b56e0da557bf1300b18d
John Seong <wonmor@gmail.com>
2022-08-18 12:37:35 +0900

    Updated train.py

--- server/train.py
+++ server/train.py
@@ -1,74 +1,41 @@
-
-import json 
-import numpy as np 
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import numpy as np
+import tflearn
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.preprocessing import LabelEncoder
-
-
-with open('intents.json') as file:
-    data = json.load(file)
-    
-training_sentences = []
-training_labels = []
-labels = []
-responses = []
-
-
-for intent in data['intents']:
-    for pattern in intent['patterns']:
-        training_sentences.append(pattern)
-        training_labels.append(intent['tag'])
-    responses.append(intent['responses'])
-    
-    if intent['tag'] not in labels:
-        labels.append(intent['tag'])
-        
-num_classes = len(labels)
-
-lbl_encoder = LabelEncoder()
-lbl_encoder.fit(training_labels)
-training_labels = lbl_encoder.transform(training_labels)
-
-vocab_size = 1000
-embedding_dim = 16
-max_len = 20
-oov_token = "<OOV>"
-
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
-tokenizer.fit_on_texts(training_sentences)
-word_index = tokenizer.word_index
-sequences = tokenizer.texts_to_sequences(training_sentences)
-padded_sequences = pad_sequences(sequences, truncating='post', maxlen=max_len)
-
-model = Sequential()
-model.add(Embedding(vocab_size, embedding_dim, input_length=max_len))
-model.add(GlobalAveragePooling1D())
-model.add(Dense(16, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
-
-model.compile(loss='sparse_categorical_crossentropy', 
-              optimizer='adam', metrics=['accuracy'])
-
-model.summary()
-
-epochs = 500
-history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
-
-# to save the trained model
-model.save("chat_model")
-
+import json
 import pickle
+import random
 
-# to save the fitted tokenizer
-with open('tokenizer.pickle', 'wb') as handle:
-    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
-# to save the fitted label encoder
-with open('label_encoder.pickle', 'wb') as ecn_file:
-    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
+class Train(object):
+    def __init__(self):
+        nltk.download("punkt")
+
+    def retrieve(self):
+        #Loading intents.json
+        with open('intents.json') as intents:
+            self.data = json.load(intents)
+
+        self.stemmer = LancasterStemmer()
+
+        # getting informations from intents.json--
+        self.words = []
+        self.labels = []
+        self.x_docs = []
+        self.y_docs = []
+
+        for intent in self.data['intents']:
+            for pattern in intent['patterns']:
+                wrds = nltk.word_tokenize(pattern)
+                self.words.extend(wrds)
+                self.x_docs.append(wrds)
+                self.y_docs.append(intent['tag'])
+
+                if intent['tag'] not in self.labels:
+                    self.labels.append(intent['tag'])
+
+    def preprocess(self):
+        # Stemming the words and removing duplicate elements.
+        words = [self.stemmer.stem(w.lower()) for w in words if w not in "?"]
+        words = sorted(list(set(words)))
+        labels = sorted(labels)
