e64d08c5dce36702ea513e4999ec5b08626076c4
John Seong <wonmor@gmail.com>
2022-08-26 03:16:33 +0900

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000)) 
-    socketio.run(app, debug=True, port=port)
+    app.run(host='0.0.0.0', port=port)
+    # socketio.run(app, debug=True, port=port)
 
 # https://strapi.io/blog/how-to-create-a-chat-bot-assistant-using-next-js-tailwind-css-and-strapi?utm_source=reddit&utm_medium=nextjs&utm_campaign=blog
